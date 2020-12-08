ea27b727be74f8ee6a0eef41e349570f55dbc1b4
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-07 23:00:59 -0500

    added php includes

--- index.php
+++ index.php
@@ -127,14 +127,14 @@
         <div id="mc_embed_signup">
         <form action="https://breadoflifediscipleship.us19.list-manage.com/subscribe/post?u=5b34198e06a16c083aa818b47&amp;id=77d274c348" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
-            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>-->
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
             <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <!-- <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5b34198e06a16c083aa818b47_77d274c348" tabindex="-1" value=""></div>
+             <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5b34198e06a16c083aa818b47_77d274c348" tabindex="-1" value=""></div>
             <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
             </div>
         </form>
         </div>  
-<!- End mc_embed_signup-->
+<!-- End mc_embed_signup-->
     </div>
   </section> 
 </div>
