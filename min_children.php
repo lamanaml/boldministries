133bf2c3d230c82fde76e6b19a056503602c588b
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-08 02:19:01 -0500

    added prayer request form

--- min_children.php
+++ min_children.php
@@ -38,13 +38,13 @@
 <!-- Body -->
 <div class="container">
     
-  <section class="py-5">
+  <!-- <section class="py-5">
     <div class="container">
       <h1 class="display-4">Weekly Lessons  </h1>
-        <!-- <p class="lead">Watch previously recorded sessions</p> -->
+       
 
     </div>
-  </section>
+  </section> -->
 
     <div class="row">
       <div class="col-lg-6 col-sm-6 mb-4">
